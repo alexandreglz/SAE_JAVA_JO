ebdaa1136613cddf6e91a4818c26b5a7b37131cf
alexandreglz <alexandre.gleizes@etu.univ-orleans.fr>
2024-04-09 15:13:09 +0200

    equipe

--- EpreuveIndividuelle.java
+++ EpreuveIndividuelle.java
@@ -11,4 +11,4 @@ public class EpreuveIndividuelle extends Epreuves{
 
     
 }
-}
+
--- src/Equipe.java
+++ src/Equipe.java
@@ -1,3 +1,29 @@
-public class Equipe {
+public class Equipe(){
+
+    private String nomEq; 
+    private int nbJoueur;
+
+
+    public clase Equipe(String nom){
+        this. nom = nom;
+    }
+
+    public void ajouterMembre(Athlete nvAthlete){
+
+    }
+
+    public void retirerMembre(Athlete Athlete){
+
+    }
+
+    public void getajouterMembre(Athlete nvAthlete){
+
+    }
+
+    public void getretirerMembre(Athlete Athlete){
     
+        }
+
+
+
 }
